f7fd2efb0cf4786904e059f4203ac0d81f994eda
alejandro-solano39 <csolano3019@gmail.com>
2024-10-23 19:24:55 -0600

    Primer etapa de organizacion del proyecto

---
+++ AugustoFlores.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="description" content="Intera - Interior HTML Template" />
+	<meta property="og:title" content="Intera - Interior HTML Template" />
+	<meta property="og:description" content="Intera - Interior HTML Template" />
+	<meta property="og:image" content="" />
+	<meta name="format-detection" content="telephone=no">
+
+	<!-- FAVICONS ICON -->
+	<link rel="icon" href="images/Vionslg.png" type="image/x-icon" />
+	<link rel="shortcut icon" type="image/x-icon" href="images/Vionslg.png" />
+
+	<!-- PAGE TITLE HERE -->
+	<title>Augusto Flores</title>
+
+	<!-- MOBILE SPECIFIC -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--[if lt IE 9]>
+	<script src="js/html5shiv.min.js"></script>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	<!-- Fuentes -->
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Exo:ital,wght@0,100..900;1,100..900&family=Martian+Mono:wght@100..800&family=New+Amsterdam&family=SUSE:wght@100..800&display=swap" rel="stylesheet">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Advent+Pro:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300..700&display=swap" rel="stylesheet">
+
+	<!-- video css-->
+	<style>
+		/* Estilos para el contenedor */
+		.containervid {
+			position: relative;
+			width: 400px;
+			height: 580px;
+			background-color: black;
+			/* Cuadro negro */
+		}
+
+		.videopxh {
+			position: absolute;
+			top: 0;
+			left: 0;
+			width: 400px;
+			height: 600px;
+			object-fit: cover;
+		}
+	</style>
+
+	<!-- STYLESHEETS -->
+	<link rel="stylesheet" type="text/css" href="css/plugins.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/templete.css">
+	<link class="skin" rel="stylesheet" type="text/css" href="css/skin/skin-1.css">
+</head>
+
+<body id="bg">
+	<div class="page-wraper">
+		<div id="loading-area"></div>
+		<!-- header -->
+		<header class="site-header header-transparent mo-left">
+			<!-- main header -->
+			<?php include('includes/navbar.php'); ?>
+			<!-- main header END -->
+		</header>
+		<!-- header END -->
+		<!-- Content -->
+		<div class="page-content">
+			<!-- inner page banner -->
+			<div class="dlab-bnr-inr dlab-bnr-inr-sm overlay-black-middle bg-pt" style="background-image:url(images/Diseñadores/AugustoFl/Aug.png);">
+				<div class="container">
+					<div class="dlab-bnr-inr-entry">
+						<h1 class="text-white">Augusto Flores</h1>
+					</div>
+				</div>
+			</div>
+			<!-- inner page banner END -->
+			<!-- Our Gallery -->
+			<div class="section-full content-inner-1 mfp-gallery">
+				<div class="container">
+					<div class="row our-gallery">
+						<div class="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.4s">
+							<div class="dlab-box portfolio-bx style2 project-media">
+								<div class="dlab-media dlab-img-overlay1 dlab-img-effect">
+									<a href="javascript:void(0);"> <img src="images/Diseñadores/AugustoFl/pic2.png" alt=""> </a>
+									<div class="overlay-bx">
+										<a href="images/Diseñadores/AugustoFl/pic2.png" class="mfp-link" title="Title Come Here"><i class="ti-zoom-in"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.6s">
+							<div class="dlab-box portfolio-bx style2 project-media">
+								<div class="dlab-media dlab-img-overlay1 dlab-img-effect">
+									<a href="javascript:void(0);"> <img src="images/Diseñadores/AugustoFl/pic1.jpg" alt=""> </a>
+									<div class="overlay-bx">
+										<a href="images/Diseñadores/AugustoFl/pic1.jpg" class="mfp-link" title="Title Come Here"><i class="ti-zoom-in"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Our Gallery END -->
+			<!-- Project Video Section-->
+			<div class="section-full content-inner-1">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6 m-b30">
+							<div class="section-head mb-0">
+								<h2 class="head-title">Digital Artist; Augusto Flores</h2>
+								<span class="pre-title">Since 2018</span>
+							</div>
+						</div>
+						<div class=" m-b30 align-self-center">
+							<p class="m-b0">At VIONS, we are dedicated to creating digital marketing that goes beyond the conventional, and our designer Augusto embodies this philosophy perfectly. Augusto is a talented graphic designer with a unique focus on innovation and futurism. His work explores rarely seen backgrounds and figures, creating visual concepts that challenge norms and open new frontiers in design.</p>
+
+							<p>a passion for experimentation and an eye for detail, Augusto transforms abstract ideas into works of visual art that capture the imagination and leave a lasting impression. His creative approach and his ability to merge unexpected elements make him a fundamental pillar of the VIONS team, helping us offer marketing solutions that stand out in the industry.</p>
+
+							<p>In his portfolio, you will find a collection of projects that reflect his commitment to innovation and his ability to anticipate and establish trends in graphic design. Each piece is a testament to their talent and dedication to pushing the boundaries of design, providing our clients with a competitive advantage in a saturated market.</p>
+
+							<p>Discover how Augusto and VIONS are redefining digital marketing and taking graphic design to new heights.</p>
+						</div>
+					</div>
+					<div class="row m-b30 widget widget_getintuch widget_getintuch-pro-details pro-details-col-5 m-lr0">
+						<div class="col p-lr0">
+							<div class="pro-details">
+								<i class="ti ti-user"></i>
+								<strong>Contact</strong>augustoflores@vions.com.mx
+							</div>
+						</div>
+						<div class="col p-lr0">
+							<div class="pro-details">
+								<i class="ti ti-location-pin"></i>
+								<strong>LOCATION</strong>Mexico, CDMX
+							</div>
+						</div>
+
+						<div class="col p-lr0">
+							<div class="pro-details">
+								<i class="ti ti-ruler-alt-2"></i>
+								<strong>SPECIALTY</strong>Deco Art Visual 3D
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- video -->
+			<div class="section-full content-inner-1 mfp-gallery">
+				<div class="container">
+					<div class="row our-gallery">
+						<div class="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInLeft">
+							<div class="dlab-box portfolio-bx style2 project-media">
+								<div class="containervid ">
+									<video class="videopxh	" autoplay muted loop>
+										<source src="images/Diseñadores/AugustoFl/vid1.mp4" type="video/mp4">
+										Tu navegador no soporta la etiqueta de video.
+									</video>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 m-b30 wow fadeInRight">
+							<div class="dlab-box portfolio-bx style2 project-media">
+								<div class="containervid ">
+									<video class="videopxh	" autoplay muted loop>
+										<source src="images/Diseñadores/AugustoFl/vid2.mp4" type="video/mp4">
+										Tu navegador no soporta la etiqueta de video.
+									</video>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 m-b30 wow fadeInRight">
+
+							<div class="containervid ">
+								<video class="videopxh	" autoplay muted loop>
+									<source src="images/Diseñadores/AugustoFl/vid3.mp4" type="video/mp4">
+									Tu navegador no soporta la etiqueta de video.
+								</video>
+							</div>
+
+						</div>
+						<div class="col-lg-6 m-b30 wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.6s">
+							<div class="containervid ">
+								<video class="videopxh	" autoplay muted loop>
+									<source src="images/Diseñadores/AugustoFl/vid4.mp4" type="video/mp4">
+									Tu navegador no soporta la etiqueta de video.
+								</video>
+							</div>|
+
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- video End -->
+			<!-- Project Video Section End -->
+			<div class="section-full content-inner-1 mfp-gallery">
+				<div class="container">
+					<div class="row our-gallery">
+						<div class="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInLeft" data-wow-duration="2s" data-wow-delay="0.4s">
+							<div class="dlab-box portfolio-bx style2 project-media">
+								<div class="dlab-media dlab-img-overlay1 dlab-img-effect">
+									<a href="javascript:void(0);"> <img src="images/Diseñadores/AugustoFl/pic3.jpeg" alt=""> </a>
+									<div class="overlay-bx">
+										<a href="images/Diseñadores/AugustoFl/pic3.jpeg" class="mfp-link" title="Title Come Here"><i class="ti-zoom-in"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6 col-md-6 col-sm-6 m-b30 wow fadeInRight" data-wow-duration="2s" data-wow-delay="0.6s">
+							<div class="dlab-box portfolio-bx style2 project-media">
+								<div class="dlab-media dlab-img-overlay1 dlab-img-effect">
+									<a href="javascript:void(0);"> <img src="images/Diseñadores/AugustoFl/pic5.jpg" alt=""> </a>
+									<div class="overlay-bx">
+										<a href="images/Diseñadores/AugustoFl/pic5.jpg" class="mfp-link" title="Title Come Here"><i class="ti-zoom-in"></i></a>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="section-head text-center">
+				<span class="pre-title">Inspired by what you see? Your project deserves the VIONS touch. Connect with us now and let's create something extraordinary!</span>
+			</div>
+			<!-- contact area END -->
+		</div>
+	</div>
+	<!-- Content END-->
+	<!-- Footer -->
+	<?php include('includes/footer.php'); ?>
+	<!-- Footer END -->
+	<!-- JAVASCRIPT FILES ========================================= -->
+	<script src="js/jquery.min.js"></script><!-- JQUERY.MIN JS -->
+	<script src="plugins/wow/wow.js"></script><!-- WOW JS -->
+	<script src="plugins/bootstrap/js/popper.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+	<script src="plugins/bootstrap/js/bootstrap.min.js"></script><!-- BOOTSTRAP.MIN JS -->
+	<script src="plugins/bootstrap-select/bootstrap-select.min.js"></script><!-- FORM JS -->
+	<script src="plugins/bootstrap-touchspin/jquery.bootstrap-touchspin.js"></script><!-- FORM JS -->
+	<script src="plugins/magnific-popup/magnific-popup.js"></script><!-- MAGNIFIC POPUP JS -->
+	<script src="plugins/counter/waypoints-min.js"></script><!-- WAYPOINTS JS -->
+	<script src="plugins/counter/counterup.min.js"></script><!-- COUNTERUP JS -->
+	<script src="plugins/imagesloaded/imagesloaded.js"></script><!-- IMAGESLOADED -->
+	<script src="plugins/masonry/masonry-3.1.4.js"></script><!-- MASONRY -->
+	<script src="plugins/masonry/masonry.filter.js"></script><!-- MASONRY -->
+	<script src="plugins/owl-carousel/owl.carousel.js"></script><!-- OWL SLIDER -->
+	<script src="js/custom.js"></script><!-- CUSTOM FUCTIONS  -->
+	<script src="js/dz.carousel.js"></script><!-- SORTCODE FUCTIONS -->
+	<script src="js/dz.ajax.js"></script><!-- CONTACT JS  -->
+
+</body>
+
+</html>
---
+++ includes/navbar.php
@@ -0,0 +1,59 @@
+    <!-- main header -->
+    <nav class="sticky-header main-bar-wraper navbar-expand-lg">
+        <div class="main-bar clearfix">
+            <div class="container clearfix">
+                <!-- website logo -->
+                <a href="/" class="logo-header mostion">
+                    <img src="../images/Vions.png" alt="Website Logo">
+                </a>
+                <!-- nav toggle button -->
+                <button class="navbar-toggler collapsed navicon justify-content-end" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </button>
+                <!-- Navigation Links -->
+                <div class="header-nav navbar-collapse collapse justify-content-end" id="navbarNavDropdown">
+                    <a href="index.html" class="logo-header">
+                        <img src="images/about/pic1.2.png" alt="Secondary Logo">
+                    </a>
+
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link">Home</a>
+                        </li>
+
+
+                        <li class="nav-item">
+                            <a href="javascript:void(0);" class="nav-link">About Us <i class="fa fa-chevron-down"></i></a>
+                            <ul class="sub-menu">
+                                <li><a href="CompanyHistory.php" class="dropdown-item">Company History</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0);" class="nav-link">Services <i class="fa fa-chevron-down"></i></a>
+                            <ul class="sub-menu">
+                                <li><a href="#" class="dropdown-item">Service</a></li>
+                                <li><a href="../" class="dropdown-item">Formulario</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0);" class="nav-link">Portfolios <i class="fa fa-chevron-down"></i></a>
+                            <ul class="sub-menu">
+                                <li><a href="#" class="dropdown-item">Designers</a></li>
+                                <li><a href="portfolio.php" class="dropdown-item">Portfolio</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="javascript:void(0);" class="nav-link">Contact Us <i class="fa fa-chevron-down"></i></a>
+                            <ul class="sub-menu">
+                                <li><a href="#" class="dropdown-item">Contact Us 01</a></li>
+                                <li><a href="#" class="dropdown-item">Contact Us 02</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- main header END -->
